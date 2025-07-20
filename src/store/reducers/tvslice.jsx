d9d60e1e134461ccddcbe3a971dbdaf983fa5caf
Shrutimaheshwari70 <Shrutimaheshwari1255@Gmail.com>
2025-07-20 18:05:18 +0530

    fix: correct tvSlice filename casing

--- src/store/reducers/tvslice.jsx
+++ src/store/reducers/tvslice.jsx
@@ -2,24 +2,22 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   info: null,
-}
-
-
+};
 
 export const tvSlice = createSlice({
-  name: 'tv',
+  name: "tv",
   initialState,
   reducers: {
     loadtv: (state, action) => {
-      state.info = action.payload
+      state.info = action.payload;
     },
-    removetv: (state, action) => {
-      state.info = null
-    }
+    removetv: (state) => {
+      state.info = null;
+    },
   },
-})
+});
 
-// Action creators are generated for each case reducer function
-export const { loadtv, removetv } = tvSlice.actions
+// Action creators generated from reducers
+export const { loadtv, removetv } = tvSlice.actions;
 
-export default tvSlice.reducer
+export default tvSlice.reducer;
