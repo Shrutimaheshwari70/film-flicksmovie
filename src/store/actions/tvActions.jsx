deb4b89691a8449be3e0cb17ffc1bb424952c33f
Shrutimaheshwari70 <Shrutimaheshwari1255@Gmail.com>
2025-07-20 17:52:43 +0530

    Fix: Corrected import in tvActions

--- src/store/actions/tvActions.jsx
+++ src/store/actions/tvActions.jsx
@@ -1,6 +1,5 @@
-export { removetv } from "../reducers/tvSlice";
+import { loadtv, removetv } from "../reducers/tvSlice";
 import axios from "../../utils/Axios";
-import { loadtv } from "../reducers/tvSlice";
 
 export const asyncloadtv = (id) => async (dispatch, getState) => {
   try {
@@ -15,17 +14,14 @@ export const asyncloadtv = (id) => async (dispatch, getState) => {
     let theultimatedetails = {
       detail: detail.data,
       externalid: externalid.data,
-      // recommendations: recommendations.data,
       recommendations: recommendations.data.results,
       similar: similar.data.results,
       translations: translations.data.translations.map((t) => t.english_name),
       videos: videos.data.results.find((m) => m.type === "Trailer"),
-
-      // videos: videos.data,
       watchproviders: watchproviders.data.results.IN,
     };
+
     dispatch(loadtv(theultimatedetails));
-    // console.log(theultimatedetails);
   } catch (err) {
     console.log("Error", err);
   }
